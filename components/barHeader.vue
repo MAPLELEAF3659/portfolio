d5df7a6795579af796fd65bfdfeae53de6e6169d
MAPLELEAF3659 <ig910425@gmail.com>
2025-03-17 20:24:08 +0800

    Add header page links

--- components/barHeader.vue
+++ components/barHeader.vue
@@ -1,18 +1,31 @@
 <template>
     <header class="header fixed h-16 w-full bg-color-header-footer z-10 p-2">
-        <div class="container h-full mx-auto flex flex-row justify-between">
-            <div class="flex h-full items-center">
+        <div class="container h-full mx-auto flex md:grid grid-cols-2 lg:grid-cols-3">
+            <div class="col-span-1 flex h-full items-center">
                 <nuxt-link class="logo" to="/">
                     <h1 class="text-title">Jun-Ye Zhou Portfolio</h1>
                 </nuxt-link>
             </div>
-            <div class="hidden md:flex h-full items-center">
+            <div class="col-span-1 hidden md:flex h-full w-full items-center justify-end lg:justify-center">
                 <ul class="flex flex-row gap-4">
                     <li>
-                        <a class="text-white" href="https://github.com/MAPLELEAF3659">GitHub</a>
+                        <nuxt-link to="/projects" class="text-desc">專案</nuxt-link>
                     </li>
                     <li>
-                        <a class="text-white" href="https://mapleleaf3659.github.io/ml-blog">Blog</a>
+                        <nuxt-link to="/about" class="text-desc">關於</nuxt-link>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-span-1 hidden lg:flex h-full items-center justify-end">
+                <ul class="flex flex-row gap-4">
+                    <li>
+                        <a class="text-desc" href="https://github.com/MAPLELEAF3659">GitHub</a>
+                    </li>
+                    <li>
+                        <a class="text-desc" href="https://mapleleaf3659.github.io/ml-blog">Blog</a>
+                    </li>
+                    <li>
+                        <nuxt-link to="/contact" class="text-desc rounded-2xl bg-slate-500 px-4 py-1">聯絡</nuxt-link>
                     </li>
                 </ul>
             </div>
