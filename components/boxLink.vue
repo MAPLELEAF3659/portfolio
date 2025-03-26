576e24f4fd11b12682445830c27ac1573d59feb6
MAPLELEAF3659 <ig910425@gmail.com>
2025-03-26 20:55:12 +0800

    Add link box

---
+++ components/boxLink.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="box-link bg-color-link-box border-black border rounded-2xl px-4 py-2 mx-auto flex flex-col">
+        <span class="text-caption">{{ caption }}</span>
+        <a :href="url" class="text-caption underline">{{ url }}</a>
+    </div>
+</template>
+<script setup lang="ts">
+defineProps({
+    caption: {
+        type: String,
+        default: ""
+    },
+    url: {
+        type: String,
+        default: ""
+    }
+})
+</script>
