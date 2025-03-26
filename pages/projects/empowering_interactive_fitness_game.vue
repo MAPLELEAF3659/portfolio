da2f1f73088a801da4201548c53d539525c1a1f0
MAPLELEAF3659 <ig910425@gmail.com>
2025-03-26 20:46:35 +0800

    Add image with caption component

--- pages/projects/empowering_interactive_fitness_game.vue
+++ pages/projects/empowering_interactive_fitness_game.vue
@@ -10,8 +10,14 @@
                     :keywords="['Artificial Intelligence', 'Fitness Game', 'Player Participation', 'Exergame']"
                     class="col-span-2 mx-auto" />
                 <CompTitleWithDesc title="摘要" title-bg="ABSTRACT"
-                    desc="此研究目的是降低現代電子健身遊戲中常見的重複性動作所帶來的無趣，並提升玩家的參與度。我們的研究提出了一種賦能式人工智慧(Empowering AI)，能夠透過體感裝置收集玩家在健身活動期間的數據，以此來增強遊戲中AI 的能力，使健身遊戲更多元化和具有挑戰性。同時，我們使用 GPT-3 來根據玩家需求生成適合的對手，提供健身活動的預評估。研究結果( 內在感知量表IMI、t-test) 顯示所提出之賦能式互動健身遊戲的玩家參與度有16% 顯著提升效果。" 
-                    class="col-span-2"/>
+                    desc="此研究目的是降低現代電子健身遊戲中常見的重複性動作所帶來的無趣，並提升玩家的參與度。我們的研究提出了一種賦能式人工智慧(Empowering AI)，能夠透過體感裝置收集玩家在健身活動期間的數據，以此來增強遊戲中AI 的能力，使健身遊戲更多元化和具有挑戰性。同時，我們使用 GPT-3 來根據玩家需求生成適合的對手，提供健身活動的預評估。研究結果( 內在感知量表IMI、t-test) 顯示所提出之賦能式互動健身遊戲的玩家參與度有16% 顯著提升效果。"
+                    class="col-span-1" />
+                <CompImgWithCaption img-url="/images/projects/empowering_interactive_fitness_game/preview.png"
+                    caption="互動系統示意圖" class="col-span-1" />
+                <CompImgWithCaption img-url="/images/projects/empowering_interactive_fitness_game/hardware.png"
+                    caption="硬體裝置示意圖" class="col-span-1" />
+                <CompImgWithCaption img-url="/images/projects/empowering_interactive_fitness_game/oral_presentation.png"
+                    caption="現場英文報告" class="col-span-1" />
             </div>
         </div>
     </div>
@@ -22,4 +28,5 @@ import SectSubHeader from '~/components/sectSubHeader.vue'
 import CompDesc from '~/components/compDesc.vue';
 import CompKeywords from '~/components/projects/compKeywords.vue';
 import CompTitleWithDesc from '~/components/compTitleWithDesc.vue';
+import CompImgWithCaption from '~/components/compImgWithCaption.vue';
 </script>
